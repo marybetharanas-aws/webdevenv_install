0d4cddda98b295dd05716d5089ab05d19b9d18b0
root <marybetharanas.aws@example.com>
2020-05-28 19:38:00 +0800

    Updated with MongoDB and Node Installation method

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@
 
 # Get installation files for inside the folder
 sublimemerge="$(ls | grep sublime-merge_build*amd64.deb)"
+mongodb="$(ls | grep mongodb-src*.tar.gz)"
 mongodbcompass="$(ls | grep mongodb-compass-community*amd64.deb)"
 
 # The packages to be installed
-apps="sublime-text mongodb node-mongodb"
+apps="sublime-text curl libcurl4 openssl"
 
 # Sublime installation prep
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -20,8 +21,20 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update
 sudo apt install $apps -y
 
+# Install NodeJS
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 # Install Sublime Merge (an alternate for Github Desktop)
 sudo dpkg -i ./$sublimemerge
+
+# Install MongoDB
+sudo tar -zxvf $mongodb
+sudo mkdir -p /var/lib/mongo
+sudo mkdir -p /var/log/mongodb
+sudo chown $USER /var/lib/mongo
+sudo chown $USER /var/log/mongodb 
+sudo mkdir -p /data/db
 
 # Install MongoDB Compass
 sudo dpkg -i ./$mongodbcompass
